bc462dc951d01996ae628c8d2e8f0169d6728f4b
px-ao <151860862+px-ao@users.noreply.github.com>
2024-06-25 17:38:53 -0300

    Add files via upload

---
+++ bm/bm.js
@@ -0,0 +1,18 @@
+//const images = document.querySelectorAll('.slideshow img');
+let currentIndex = 0;
+setInterval(function() {
+    alert("Passou um sengundo")
+    console.log("Olá 1");
+}, 1000);
+
+/* function showSlide() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+} */
+
+// Altere os slides a cada 2 segundos
+setInterval(function() {
+    
+    alert("Tempo 2");
+}, 1000);
